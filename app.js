1554fb8666f60ec1dc7ce5d2b0a11c199ff5b63a
jmclella <jmclella@fandm.edu>
2022-12-18 17:10:10 -0500

    Create HTML/CSS elements for gameboard
    Begun to add JS variables for gameboard and cells

---
+++ app.js
@@ -0,0 +1,18 @@
+// Create gameBoard array
+let gameBoard = ['X', 'X', 'X', 'X', 'X', 'X','X', 'X', 'X'];
+
+// Declare grid cell variables
+const cell0 = document.getElementById('cell0');
+const cell1 = document.getElementById('cell1');
+const cell2 = document.getElementById('cell2');
+const cell3 = document.getElementById('cell3');
+const cell4 = document.getElementById('cell4');
+const cell5 = document.getElementById('cell5');
+const cell6 = document.getElementById('cell6');
+const cell7 = document.getElementById('cell7');
+const cell8 = document.getElementById('cell8');
+
+
+const genBoard = (function () {
+    
+}) ();
