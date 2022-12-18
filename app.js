cc66f0fadb96d8f600c480a91738a7d70d797880
jmclella <jmclella@fandm.edu>
2022-12-18 17:37:35 -0500

    Added basic javascript functionality to genBoard

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 // Create gameBoard array
-let gameBoard = ['X', 'X', 'X', 'X', 'X', 'X','X', 'X', 'X'];
+let gameBoard = ['X', 'X', 'O', 'X', 'X', 'X','X', 'X', 'X'];
 
 // Declare grid cell variables
 const cell0 = document.getElementById('cell0');
@@ -13,6 +13,32 @@ const cell7 = document.getElementById('cell7');
 const cell8 = document.getElementById('cell8');
 
 
-const genBoard = (function () {
-    
+const displayController = (function () {
+    const genBoard = () => {
+        cell0.textContent = gameBoard[0];
+        cell1.textContent = gameBoard[1];
+        cell2.textContent = gameBoard[2];
+        cell3.textContent = gameBoard[3];
+        cell4.textContent = gameBoard[4];
+        cell5.textContent = gameBoard[5];
+        cell6.textContent = gameBoard[6];
+        cell7.textContent = gameBoard[7];
+        cell8.textContent = gameBoard[8];
+    }
+
+    const choiceX = () => {
+        gameBoard = ['', '', '', '', '', '', '', '', ''];
+        genBoard();
+    }
+
+    const choiceO = () => {
+        let randomChoice = Math.floor(Math.random() * 9);
+        gameBoard = ['', '', '', '', '', '', '', '', ''];
+        gameBoard[randomChoice] = 'O';
+        genBoard();
+    }
+
+    return {genBoard, choiceX, choiceO};
 }) ();
+
+displayController.genBoard();
